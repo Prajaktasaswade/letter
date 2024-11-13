4056bee24fc3c6b2ae4118ed04c1d4cd79730f73
Prajaktasaswade <127827390+Prajaktasaswade@users.noreply.github.com>
2024-11-13 12:42:22 +0530

    Update settings.py

--- hr_documents_project/settings.py
+++ hr_documents_project/settings.py
@@ -35,6 +35,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
 ]
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "https://prajaktasaswade.github.io",  # GitHub Pages domain
 ]
