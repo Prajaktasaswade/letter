a2fe68803d732d0fc444f06a22f870068c1d0122
Prajaktasaswade <127827390+Prajaktasaswade@users.noreply.github.com>
2024-11-14 15:18:53 +0530

    Update urls.py

--- hr_documents_project/urls.py
+++ hr_documents_project/urls.py
@@ -4,6 +4,7 @@ from letters import views  # Import views from your app
 
 urlpatterns = [
     path('admin/', admin.site.urls),  # Admin URL
+    path('', views.index_page, name='index_page'),  # Your home page URL
     path('letters/', include('letters.urls')),  # Include URLs from the 'letters' app
     path('index-letter/', views.index_letter_view, name='index_letter'),
    
